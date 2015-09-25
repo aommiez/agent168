1ca28e3c9752e2abce55dd44e4aa4a911e929057
nuiz <papangping@gmail.com>
2015-09-25 11:32:34 +0700

    phone req

---
+++ public/app/phonereq/app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by NuizHome on 8/4/2558.
+ */
+
+var app = angular.module('enquiry-app', ['ngRoute', 'angular-loading-bar']);
+app.config(['$routeProvider', 'cfpLoadingBarProvider',
+    function($routeProvider, cfpLoadingBarProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: '../public/app/phonereq/list.html'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
+app.controller('ListCTL', ['$scope', '$http', '$location', '$route', function($scope, $http, $location, $route){
+
+}]);
